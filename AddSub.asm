e5669d1a74a155a39fa32cc04028c32cc782bbbd
tenhagena <tenhagena@WITC02S24RMG8WM>
2016-11-02 15:13:05 -0400

    Add project files.

---
+++ AddSub.asm
@@ -0,0 +1,17 @@
+﻿TITLE MASM Template						(main.asm)
+
+INCLUDE Irvine32.inc
+.data
+myMessage BYTE "Hello World",0dh,0ah,0
+
+.code
+main PROC
+	call Clrscr
+
+	mov	 edx,OFFSET myMessage
+	call WriteString
+
+	exit
+main ENDP
+
+END main
---
+++ main.asm
@@ -0,0 +1,137 @@
+TITLE andy		(arrayweek.asm)
+INCLUDE Irvine32.inc
+.data
+line1 db "############################",0
+line2 db "#oooooooooooo##oooooooooooo#",0
+line3 db "#o####o#####o##o#####o####o#",0
+line4 db "#o####o#####o##o#####o####o#",0
+line5 db "#o####o#####o##o#####o####o#",0
+line6 db "#oooooooooooooooooooooooooo#",0
+line7 db "#o####o##o########o##o####o#",0
+line8 db "#o####o##o########o##o####o#",0
+line9 db "#oooooo##oooo##oooo##oooooo#",0
+lineA db "######o#####o##o#####o######",0
+lineB db "######o#####o##o#####o######",0
+lineC db "######o##oooooooooo##o######",0
+lineD db "######o##o###__###o##o######",0
+lineE db "######o##o#______#o##o######",0
+lineF db "______oooo#______#oooo______",0
+line10 db "######o##o#______#o##o######",0
+line11 db "######o##o########o##o######",0
+line12 db "######o##oooooooooo##o######",0
+line13 db "######o##o########o##o######",0
+line14 db "######o##o########o##o######",0
+line15 db "#oooooooooooo##oooooooooooo#",0
+line16 db "#o####o#####o##o#####o####o#",0
+line17 db "#o####o#####o##o#####o####o#",0
+line18 db "#ooo##oooooooooooooooo##ooo#",0
+line19 db "###o##o##o########o##o##o###",0
+line1A db "###o##o##o########o##o##o###",0
+line1B db "#oooooo##oooo##oooo##oooooo#",0
+line1C db "#o##########o##o##########o#",0
+line1D db "#o##########o##o##########o#",0
+line1E db "#oooooooooooooooooooooooooo#",0
+line1F db "############################",0
+.code
+main PROC
+call drawstart
+exit
+main ENDP
+
+Drawstart Proc
+mov edx , offset line1
+call writestring
+call crlf
+mov edx , offset line2
+call writestring
+call crlf
+mov edx , offset line3
+call writestring
+call crlf
+mov edx , offset line4
+call writestring
+call crlf
+mov edx , offset line5
+call writestring
+call crlf
+mov edx , offset line6
+call writestring
+call crlf
+mov edx , offset line7
+call writestring
+call crlf
+mov edx , offset line8
+call writestring
+call crlf
+mov edx , offset line9
+call writestring
+call crlf
+mov edx , offset lineA
+call writestring
+call crlf
+mov edx , offset lineB
+call writestring
+call crlf
+mov edx , offset lineC
+call writestring
+call crlf
+mov edx , offset lineD
+call writestring
+call crlf
+mov edx , offset lineE
+call writestring
+call crlf
+mov edx , offset lineF
+call writestring
+call crlf
+mov edx , offset line10
+call writestring
+call crlf
+mov edx , offset line11
+call writestring
+call crlf
+mov edx , offset line12
+call writestring
+call crlf
+mov edx , offset line13
+call writestring
+call crlf
+mov edx , offset line14
+call writestring
+call crlf
+mov edx , offset line15
+call writestring
+call crlf
+mov edx , offset line16
+call writestring
+call crlf
+mov edx , offset line17
+call writestring
+call crlf
+mov edx , offset line18
+call writestring
+call crlf
+mov edx , offset line19
+call writestring
+call crlf
+mov edx , offset line1A
+call writestring
+call crlf
+mov edx , offset line1B
+call writestring
+call crlf
+mov edx , offset line1C
+call writestring
+call crlf
+mov edx , offset line1D
+call writestring
+call crlf
+mov edx , offset line1E
+call writestring
+call crlf
+mov edx , offset line1F
+call writestring
+call crlf
+ret
+Drawstart endp
+END main
