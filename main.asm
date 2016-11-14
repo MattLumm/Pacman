f24515e36832ee8dee05c3503b72054036cc8458
MattLumm <lumm@wit.edu>
2016-11-14 12:15:16 -0500

    Update main.asm
    I added spawnpacman, movpacright,left,up,down. Also added a table case and setline as helpers.
    I still need to add collision and the teleporter function.
    Also needs to count when it collects a pellet and needs to be in a loop that continuously calls it's mov procedures.

--- main.asm
+++ main.asm
@@ -1,6 +1,7 @@
 TITLE andy		(arrayweek.asm)
 INCLUDE Irvine32.inc
 .data
+; board is 31X28; err now 31X56 i think didn't count
 winning byte 0
 line1 db "#######################################################",0
 line2 db "# o o o o o o o o o o o o ### o o o o o o o o o o o o #",0
@@ -33,10 +34,92 @@ line1C db "# o ################### o ### o ################### o #",0
 line1D db "# o ################### o ### o ################### o #",0
 line1E db "# o o o o o o o o o o o o o o o o o o o o o o o o o o #",0
 line1F db "#######################################################",0
+
+CaseTable BYTE 1 ; lookup Value
+	DWORD Process_1 ;address of procedure
+EntrySize = ($ - CaseTable)
+		BYTE 2
+	DWORD Process_2
+		BYTE 3
+	DWORD Process_3
+		BYTE 4
+	DWORD Process_4
+		BYTE 5
+	DWORD Process_5
+		BYTE 6
+	DWORD Process_6
+		BYTE 7
+	DWORD Process_7
+		BYTE 8
+	DWORD Process_8
+		BYTE 9
+	DWORD Process_9
+		BYTE 10
+	DWORD Process_10
+		BYTE 11
+	DWORD Process_11
+		BYTE 12
+	DWORD Process_12
+		BYTE 13
+	DWORD Process_13
+		BYTE 14
+	DWORD Process_14
+		BYTE 15
+	DWORD Process_15
+		BYTE 16
+	DWORD Process_16
+		BYTE 17
+	DWORD Process_17
+		BYTE 18
+	DWORD Process_18
+		BYTE 19
+	DWORD Process_19
+		BYTE 20
+	DWORD Process_20
+		BYTE 21
+	DWORD Process_21
+		BYTE 22
+	DWORD Process_22
+		BYTE 23
+	DWORD Process_23
+		BYTE 24
+	DWORD Process_24
+		BYTE 25
+	DWORD Process_25
+		BYTE 26
+	DWORD Process_26
+		BYTE 27
+	DWORD Process_27
+		BYTE 28
+	DWORD Process_28
+		BYTE 29
+	DWORD Process_29
+		BYTE 30
+	DWORD Process_30
+		BYTE 31
+	DWORD Process_31
+
+
+NumberOfEntries = ($-CaseTable) / EntrySize
+
+temptest db ?
 .code
 main PROC
+	mov eax, 0
+	mov ebx, 0
 	call drawstart
-	mainloop:
+	call spawnpac
+	call drawstart
+	call movpacright
+	call drawstart
+	call movpacup
+	call drawstart
+	call movpacleft
+	call drawstart
+	call movpacdown
+	call drawstart
+
+mainloop:
 	call drawdots
 	call drawpac
 	call updatedots
@@ -163,4 +246,239 @@ call writestring
 call crlf
 ret
 Drawstart endp
+
+spawnpac proc USES esi eax
+; pac man uses <, >, ^, v  depending which direction he's heading starts heading right
+.data 
+pacX dd 0
+pacY db 0
+pacDir db 0
+.code
+; this will just put pacman into the board at 12X13 aka lineC at index 14
+	mov pacY, 12
+	;mov esi, offset lineC
+	Call setline
+	mov pacx, 28
+	add esi, pacx
+	mov al, '<'
+	mov [esi], al
+	ret
+spawnpac endp
+
+movpacright proc USES esi eax
+; need to check if the next spot is open or not
+; set pacDir = 0 to rep pac heading right, replace pacX, pacY with _ to represent pellet eaten, then inc pacX, move pac to pacX, pacY 
+	mov pacDir, 0
+	Call setline
+	add esi, pacX
+	mov al, '_'
+	mov [esi], al
+	inc esi
+	inc esi
+	inc pacX
+	inc pacX
+	mov al, '<'
+	mov [esi], al
+ret
+movpacright endp
+
+movpacleft proc USES esi eax
+; need to check if the next spot is open or not
+; set pacDir = 1 to rep pac heading left, replace pacX, pacY with _ to represent pellet eaten, then dec pacX, move pac to pacX, pacY 
+	mov pacDir, 1
+	Call setline
+	add esi, pacX
+	mov al, '_'
+	mov [esi], al
+	dec esi
+	dec esi
+	dec pacX
+	dec pacX
+	mov al, '<'
+	mov [esi], al
+ret
+movpacleft endp
+
+movpacup proc USES esi eax
+; need to check if the next spot is open still
+; set pacDir = 2 to rep pac heading up, replace pacX, pacY with _, then dec pacY, mov pac to pacX, pacY
+	mov pacDir, 2
+	Call setline
+	add esi, pacX
+	mov al,'_'
+	mov [esi], al
+	dec pacY
+	Call setline
+	add esi, pacX
+	mov al, '^'
+	mov [esi], al
+ret
+movpacup endp
+
+movpacdown proc USES esi eax
+; need to check if the next spot is open still
+; set pacDir = 4 to rep pac heading down, replace pacX, pacY with _, then inc pacY, mov pac to pacX, pacY
+	mov pacDir, 3
+	Call setline
+	add esi, pacX
+	mov al,'_'
+	mov [esi], al
+	inc pacY
+	Call setline
+	add esi, pacX
+	mov al, 'v'
+	mov [esi], al
+ret
+movpacdown endp
+
+;used to set the esi to the line of PacY
+setline proc
+;call readchar	;read char to AL
+	mov al, PacY				; so you have the right column get selected
+	mov ebx, offset CaseTable ; point ebx to the table
+	mov ecx, NumberOfEntries ; loop counter
+L1:
+mov ah, [ebx]
+	mov temptest, ah
+	cmp al, [ebx]	;match found?
+	jne L2			; no: continue
+	call NEAR PTR [ebx+1]	;yes: call procedure
+	call crlf
+	jmp L3	;exit search
+L2:
+	add ebx, EntrySize	; point to the next entry
+	loop L1				; repeat until ECX =0
+L3:
+
+ret
+setline endp
+
+Process_1 PROC
+	mov esi, offset line1
+	ret
+Process_1 ENDP
+
+Process_2 PROC
+	mov esi, offset line2
+	ret
+Process_2 ENDP
+Process_3 PROC
+	mov esi, offset line3
+	ret
+Process_3 ENDP
+Process_4 PROC
+	mov esi, offset line4
+	ret
+Process_4 ENDP
+Process_5 PROC
+	mov esi, offset line5
+	ret
+Process_5 ENDP
+Process_6 PROC
+	mov esi, offset line6
+	ret
+Process_6 ENDP
+Process_7 PROC
+	mov esi, offset line7
+	ret
+Process_7 ENDP
+Process_8 PROC
+	mov esi, offset line8
+	ret
+Process_8 ENDP
+Process_9 PROC
+	mov esi, offset line9
+	ret
+Process_9 ENDP
+Process_10 PROC
+	mov esi, offset lineA
+	ret
+Process_10 ENDP
+Process_11 PROC
+	mov esi, offset lineB
+	ret
+Process_11 ENDP
+Process_12 PROC
+	mov esi, offset lineC
+	ret
+Process_12 ENDP
+Process_13 PROC
+	mov esi, offset lineD
+	ret
+Process_13 ENDP
+Process_14 PROC
+	mov esi, offset lineE
+	ret
+Process_14 ENDP
+Process_15 PROC
+	mov esi, offset lineF
+	ret
+Process_15 ENDP
+Process_16 PROC
+	mov esi, offset line10
+	ret
+Process_16 ENDP
+Process_17 PROC
+	mov esi, offset line11
+	ret
+Process_17 ENDP
+Process_18 PROC
+	mov esi, offset line12
+	ret
+Process_18 ENDP
+Process_19 PROC
+	mov esi, offset line13
+	ret
+Process_19 ENDP
+Process_20 PROC
+	mov esi, offset line14
+	ret
+Process_20 ENDP
+Process_21 PROC
+	mov esi, offset line15
+	ret
+Process_21 ENDP
+Process_22 PROC
+	mov esi, offset line16
+	ret
+Process_22 ENDP
+Process_23 PROC
+	mov esi, offset line17
+	ret
+Process_23 ENDP
+Process_24 PROC
+	mov esi, offset line18
+	ret
+Process_24 ENDP
+Process_25 PROC
+	mov esi, offset line19
+	ret
+Process_25 ENDP
+Process_26 PROC
+	mov esi, offset line1A
+	ret
+Process_26 ENDP
+Process_27 PROC
+	mov esi, offset line1B
+	ret
+Process_27 ENDP
+Process_28 PROC
+	mov esi, offset line1C
+	ret
+Process_28 ENDP
+Process_29 PROC
+	mov esi, offset line1D
+	ret
+Process_29 ENDP
+Process_30 PROC
+	mov esi, offset line1E
+	ret
+Process_30 ENDP
+Process_31 PROC
+	mov esi, offset line1F
+	ret
+Process_31 ENDP
+
+
+
 END main
