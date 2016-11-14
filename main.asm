f6f48166037f42161ec24f8b8c9b5237edc09b45
sheehanmilesk <sheehanmilesk@WIT.edu>
2016-11-14 09:32:01 -0500

    Worked on barebone gameloop

--- main.asm
+++ main.asm
@@ -1,6 +1,7 @@
 TITLE andy		(arrayweek.asm)
 INCLUDE Irvine32.inc
 .data
+winning byte 0
 line1 db "############################",0
 line2 db "#oooooooooooo##oooooooooooo#",0
 line3 db "#o####o#####o##o#####o####o#",0
@@ -34,9 +35,37 @@ line1E db "#oooooooooooooooooooooooooo#",0
 line1F db "############################",0
 .code
 main PROC
-call drawstart
+	mainloop:
+	call drawstart
+	call drawdots
+	call drawpac
+	call updatedots
+	call input
+	cmp winning, 0
+	je mainloop
 exit
 main ENDP
+
+input proc
+	;this should get the player's input and change pacman's xy coordinates. Doesn't actually change where the picture is tho.
+	ret
+input endp
+
+drawdots proc
+	;put code to draw the dots here
+	ret
+drawdots endp
+
+drawpac proc
+	;put code to draw pacman at a specific xy coordinate here
+	ret
+drawpac endp
+
+updatedots proc
+	;code to remove any dots in the same square as pacman goes here
+	;this should also increment the score
+	ret
+updatedots endp
 
 Drawstart Proc
 mov edx , offset line1
